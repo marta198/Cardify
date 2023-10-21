289f1d2b48a9e42d042629fad79a8d6caca6211c
marta198 <95033992+marta198@users.noreply.github.com>
2023-10-21 19:16:17 +0300

    Add SignIn and SignUp activities

---
+++ app/src/main/java/com/example/cardify/SignIn.java
@@ -0,0 +1,114 @@
+package com.application.cardify;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatCheckBox;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SignIn extends AppCompatActivity {
+    TextInputEditText editEmail, editPassword;
+    Button buttonSignIn;
+    FirebaseAuth mAuth;
+    TextView textView;
+    AppCompatCheckBox checkbox;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        mAuth = FirebaseAuth.getInstance();
+        editEmail = findViewById(R.id.email_login);
+        editPassword = findViewById(R.id.password_login);
+        buttonSignIn = findViewById(R.id.sign_in_button);
+        checkbox = (AppCompatCheckBox) findViewById(R.id.checkbox);
+        textView = findViewById(R.id.sign_up_field);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SignUp.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        checkbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if (!isChecked) {
+                    // show password
+                    editPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                } else {
+                    // hide password
+                    editPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                }
+            }
+        });
+
+        buttonSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email, password;
+                email = String.valueOf(editEmail.getText());
+                password = String.valueOf(editPassword.getText());
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(SignIn.this, "Please enter your email", Toast.LENGTH_SHORT).show();
+                    return;
+                };
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(SignIn.this, "Please enter your password", Toast.LENGTH_SHORT).show();
+                    return;
+                };
+
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(SignIn.this, "Authentication successful.",
+                                            Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+                                    Toast.makeText(SignIn.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/cardify/SignUp.java
@@ -0,0 +1,113 @@
+package com.application.cardify;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatCheckBox;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SignUp extends AppCompatActivity {
+
+    TextInputEditText editEmail, editPassword;
+    Button buttonSignUp;
+    FirebaseAuth mAuth;
+    TextView textView;
+    AppCompatCheckBox checkbox;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        mAuth = FirebaseAuth.getInstance();
+        editEmail = findViewById(R.id.email_login);
+        editPassword = findViewById(R.id.password_login);
+        buttonSignUp = findViewById(R.id.sign_up_button);
+        checkbox = (AppCompatCheckBox) findViewById(R.id.checkbox);
+        textView = findViewById(R.id.sign_in_field);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SignIn.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        checkbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if (!isChecked) {
+                    // show password
+                    editPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                } else {
+                    // hide password
+                    editPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                }
+            }
+        });
+
+        buttonSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email, password;
+                email = String.valueOf(editEmail.getText());
+                password = String.valueOf(editPassword.getText());
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(SignUp.this, "Please enter your email", Toast.LENGTH_SHORT).show();
+                    return;
+                };
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(SignUp.this, "Please enter a password", Toast.LENGTH_SHORT).show();
+                    return;
+                };
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(SignUp.this, "Created account",
+                                            Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(getApplicationContext(), SignIn.class);
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(SignUp.this, "Account creation failed.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
+            }
+        });
+    }
+}
