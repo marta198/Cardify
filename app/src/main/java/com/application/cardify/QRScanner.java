4d73d452f0cd8fb99dbce01dcbfa7fb8346cfaec
marta198 <95033992+marta198@users.noreply.github.com>
2023-10-23 12:46:30 +0300

    Connecting to database

--- app/src/main/java/com/application/cardify/QRScanner.java
+++ app/src/main/java/com/application/cardify/QRScanner.java
@@ -1,56 +1,53 @@
 package com.application.cardify;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.TextView;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
 public class QRScanner extends AppCompatActivity {
 
-    Button scanButton;
-    TextView textView;
-    private String user; // New variable for the user's email
+    Button scanButton, backButton;
+
     private String fullName;
     private String companyName;
     private String email;
     private String address;
     private String phoneNumber;
     private String websiteLink;
-    private String bgImage; // Variable for background image URL
+    private String bgImage;
     private String logoLink;
-    private boolean straightTrought = false;
+    private String importance;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser currentUser;
+    private DatabaseReference cardsRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrscanner);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance("https://cardify-402213-default-rtdb.europe-west1.firebasedatabase.app/").getReference();
+        cardsRef = databaseReference.child("cards");
+        currentUser = firebaseAuth.getCurrentUser();
         scanButton = findViewById(R.id.scanButton);
-        textView = findViewById(R.id.qrCodeText);
-
-        Bundle extras = getIntent().getExtras();
-        if (extras != null) {
-            GsonBuilder builder = new GsonBuilder();
-            builder.setPrettyPrinting();
-
-            Gson gson = builder.create();
-            if (extras.getBoolean("openScanner")) {
-                straightTrought = true;
-                IntentIntegrator intentIntegrator = new IntentIntegrator(QRScanner.this);
-                intentIntegrator.setOrientationLocked(true);
-                intentIntegrator.setPrompt("Scan the trading card QR code");
-                intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
-                intentIntegrator.setBeepEnabled(false); // Disable the beep sound, can be enabled for debugging
-                intentIntegrator.initiateScan();
-            }
-        }
+        backButton = findViewById(R.id.backButton);
 
         scanButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,52 +56,113 @@ public class QRScanner extends AppCompatActivity {
                 intentIntegrator.setOrientationLocked(true);
                 intentIntegrator.setPrompt("Scan the trading card QR code");
                 intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
-                intentIntegrator.setBeepEnabled(false); // Disable the beep sound, can be enabled for debugging
+                intentIntegrator.setBeepEnabled(false); // Disable the beep sound
                 intentIntegrator.initiateScan();
+            }
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
             }
         });
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
         IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
 
-        if (straightTrought) {
-            // Handle the result directly without user interaction
-            if (intentResult != null && intentResult.getContents() != null) {
-                String contents = intentResult.getContents();
-                setResult(resultCode, getIntent().putExtra("returnData", contents));
-                finish();
-                parseQRCodeContents(contents);
-            }
-        } else {
-            // Handle the result with user interaction
-            if (intentResult != null && intentResult.getContents() != null) {
-                String contents = intentResult.getContents();
-                parseQRCodeContents(contents);
-            }
+        if (intentResult != null && intentResult.getContents() != null) {
+            String contents = intentResult.getContents();
+            parseQRCodeContents(contents);
         }
-
-        super.onActivityResult(requestCode, resultCode, data);
     }
 
     private void parseQRCodeContents(String contents) {
         if (contents != null) {
-            String[] split = contents.split("\\|");
-            if (split.length == 9) {
-                user = split[0]; // New variable for the user's email
-                fullName = split[1];
-                companyName = split[2];
-                email = split[3];
-                address = split[4];
-                phoneNumber = split[5];
-                websiteLink = split[6];
-                bgImage = split[7]; // New variable for background image URL
-                logoLink = split[8];
+            String[] split = contents.split("\n");
+            if (split.length == 8) {
+                fullName = split[0];
+                companyName = split[1];
+                email = split[2];
+                address = split[3];
+                phoneNumber = split[4];
+                websiteLink = split[5];
+                bgImage = split[6];
+                logoLink = split[7];
 
-                // Update the UI or save the data to Firebase as needed
-                textView.setText(contents);
+                // Display the data or save it to the database
+                showImportanceDialog();
+            } else {
+                showAlertDialog("Invalid QR Code Data. Please scan a valid trading card QR code.");
             }
         }
     }
+
+    private void showImportanceDialog() {
+        final String[] importanceLevels = {"Important", "Maybe", "Not Important"};
+        final Spinner importanceSpinner = new Spinner(this);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, importanceLevels);
+        importanceSpinner.setAdapter(adapter);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Select Importance Level");
+        builder.setView(importanceSpinner);
+        builder.setPositiveButton("Save Card", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                importance = importanceLevels[importanceSpinner.getSelectedItemPosition()];
+                saveCardToDatabase();
+                dialog.dismiss();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.create().show();
+    }
+
+    private void saveCardToDatabase() {
+        Card newCard = new Card(
+                currentUser.getEmail(),
+                fullName,
+                companyName,
+                email,
+                address,
+                phoneNumber,
+                websiteLink,
+                bgImage,
+                logoLink,
+                importance,
+                false
+        );
+
+        DatabaseReference newCardRef = cardsRef.push();
+        String cardKey = newCardRef.getKey();
+        newCard.setKey(cardKey);
+        newCardRef.setValue(newCard);
+
+        Toast.makeText(this, "Card saved successfully!", Toast.LENGTH_SHORT).show();
+    }
+
+    private void showAlertDialog(String message) {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setTitle("Error");
+        alertDialog.setMessage(message);
+        alertDialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        alertDialog.show();
+    }
 }
+
