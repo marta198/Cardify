17dbc59bcf452ef22646e47fa84230f3c85a5414
marta198 <95033992+marta198@users.noreply.github.com>
2023-10-23 12:46:59 +0300

    Connecting to database

--- app/src/main/java/com/application/cardify/TabCards.java
+++ app/src/main/java/com/application/cardify/TabCards.java
@@ -5,22 +5,35 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import androidx.appcompat.widget.AppCompatImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatImageButton;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import com.application.cardify.Card;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class TabCards extends Fragment {
 
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance("https://cardify-402213-default-rtdb.europe-west1.firebasedatabase.app/").getReference();
+    DatabaseReference cardsRef = databaseReference.child("cards");
     private String title;
-    private List<Card> myCurrentCards;
+    private View fragmentView;
 
     public TabCards(String title) {
         this.title = title;
@@ -33,12 +46,13 @@ public class TabCards extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_tab_cards, container, false);
+        return inflater.inflate(R.layout.fragment_tab_cards, container, true);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        fragmentView = view;
         TextView titleText = view.findViewById(R.id.MyCardsTitle);
         titleText.setText(this.title);
         AppCompatImageButton addNewBtn = view.findViewById(R.id.cardAddNew);
@@ -51,38 +65,37 @@ public class TabCards extends Fragment {
             }
         });
 
-        // Initialize the list of static cards
-        myCurrentCards = createStaticCards();
-        displayStaticCards(view);
+        createDynamicCards();
     }
 
+    public void createDynamicCards() {
+        ValueEventListener valueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                List<Card> dynamicCards = new ArrayList<>();
+                for(DataSnapshot ds : snapshot.getChildren()) {
+                    Card card = ds.getValue(Card.class);
+                    String cardKey = ds.getKey();
+                    card.setKey(cardKey);
+                    if (card.getUser().equals(currentUser.getEmail()) && card.getIsOwner()) {
+                        dynamicCards.add(card);
+                    }
+                }
+                displayDynamicCards(dynamicCards);
+            }
 
-    private List<Card> createStaticCards() {
-        List<Card> staticCards = new ArrayList<>();
-        for (int i = 1; i <= 13; i++) {
-            Card card = new Card(
-                    "User " + i,
-                    "Name " + i,
-                    "Company " + i,
-                    "Phone " + i,
-                    "Email " + i,
-                    "Website " + i,
-                    "Address " + i,
-                    "Image " + i,
-                    "Logo " + i,
-                    "Important Notes " + i,
-                    true
-            );
-            staticCards.add(card);
-        }
-
-        return staticCards;
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getContext(), "Failed to get data.", Toast.LENGTH_SHORT).show();
+            }
+        };
+        cardsRef.addValueEventListener(valueEventListener); // Use addValueEventListener instead of addListenerForSingleValueEvent
     }
 
-    private void displayStaticCards(View view) {
-        RecyclerView recyclerView = view.findViewById(R.id.cardRecyclerView);
+    public void displayDynamicCards(List<Card> dynamicCards) {
+        RecyclerView recyclerView = fragmentView.findViewById(R.id.cardRecyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        CardAdapter cardAdapter = new CardAdapter(getContext(), myCurrentCards, true);
+        CardAdapter cardAdapter = new CardAdapter(getContext(), dynamicCards, true);
         recyclerView.setAdapter(cardAdapter);
     }
 }
