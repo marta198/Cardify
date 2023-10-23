c7bca5501f17df3655aaf6be708f3b4a7a8ddd3c
marta198 <95033992+marta198@users.noreply.github.com>
2023-10-23 12:48:04 +0300

    Connecting to database

--- app/src/main/java/com/application/cardify/TabTraded.java
+++ app/src/main/java/com/application/cardify/TabTraded.java
@@ -1,6 +1,12 @@
 package com.application.cardify;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -8,14 +14,14 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -24,9 +30,16 @@ import java.util.Random;
 
 public class TabTraded extends Fragment {
 
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance("https://cardify-402213-default-rtdb.europe-west1.firebasedatabase.app/").getReference();
+    DatabaseReference cardsRef = databaseReference.child("cards");
     private String title;
     private CardAdapter cardAdapter;
-    private List<Card> myCurrentCards;
+    View fragmentView;
+    private List<Card> dynamicCards;
+
+    private Button importantBtn, maybeBtn, notImportantBtn;
     public TabTraded(String title) {
         this.title = title;
     }
@@ -43,20 +56,19 @@ public class TabTraded extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        fragmentView = view;
         TextView titleText = view.findViewById(R.id.TradedTitle);
         titleText.setText(this.title);
-        // Initialize the list of static cards
-        myCurrentCards = createStaticCards();
-        displayStaticCards(view);
 
-        Button interestingBtn = view.findViewById(R.id.InterestingChip);
-        Button maybeBtn = view.findViewById(R.id.MaybeChip);
-        Button notInterestingBtn = view.findViewById(R.id.NotInterestingChip);
+        createDynamicCards();
+
+        importantBtn = view.findViewById(R.id.Important);
+        maybeBtn = view.findViewById(R.id.MaybeChip);
+        notImportantBtn = view.findViewById(R.id.NotImportant);
         List<Button> filterButtons = new ArrayList<>();
-        filterButtons.add(interestingBtn);
+        filterButtons.add(importantBtn);
         filterButtons.add(maybeBtn);
-        filterButtons.add(notInterestingBtn);
-
+        filterButtons.add(notImportantBtn);
 
         for (Button item:filterButtons) {
             item.setOnClickListener(new View.OnClickListener() {
@@ -73,45 +85,76 @@ public class TabTraded extends Fragment {
             });
         }
 
-
-
-
-
     }
 
     List<String> choices = Arrays.asList("Interesting", "Maybe", "NotInteresting");
 
     ;
-    private List<Card> createStaticCards() {
-        List<Card> staticCards = new ArrayList<>();
-        Random rand = new Random();
-        for (int i = 1; i <= 13; i++) {;
-            int randomIndex = rand.nextInt(choices.size());
-            Card card = new Card(
-                    "User " + i,
-                    "Name " + i,
-                    "Company " + i,
-                    "Phone " + i,
-                    "Email " + i,
-                    "Website " + i,
-                    "Address " + i,
-                    "Image " + i,
-                    "Logo " + i,
-                    choices.get(randomIndex),
-                    true
-            );
-            staticCards.add(card);
-        }
 
-        return staticCards;
+    public void createDynamicCards() {
+        ValueEventListener valueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                dynamicCards = new ArrayList<>();
+                Random rand = new Random();
+                for (DataSnapshot ds : snapshot.getChildren()) {
+                    Card card = ds.getValue(Card.class);
+                    String cardKey = ds.getKey();
+                    card.setKey(cardKey);
+                    if (card.getUser().equals(currentUser.getEmail()) && !card.getIsOwner()) {
+                        int randomIndex = rand.nextInt(choices.size());
+                        card.setImportance(choices.get(randomIndex));
+                        dynamicCards.add(card);
+                    }
+                }
+                filterAndDisplayDynamicCards();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getContext(), "Failed to get data.", Toast.LENGTH_SHORT).show();
+            }
+        };
+        cardsRef.addListenerForSingleValueEvent(valueEventListener);
     }
 
-    private void displayStaticCards(View view) {
-        RecyclerView recyclerView = view.findViewById(R.id.cardRecyclerView);
+    public void displayDynamicCards(List<Card> dynamicCards) {
+        RecyclerView recyclerView = fragmentView.findViewById(R.id.cardRecyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        cardAdapter = new CardAdapter(getContext(), myCurrentCards, false);
+        cardAdapter = new CardAdapter(getContext(), dynamicCards, false);
         recyclerView.setAdapter(cardAdapter);
     }
 
+    public void filterAndDisplayDynamicCards() {
+        List<Card> filteredCards = new ArrayList<>();
 
+        for (Card card : dynamicCards) {
+            if (isCardFiltered(card)) {
+                filteredCards.add(card);
+            }
+        }
+
+        displayDynamicCards(filteredCards);
+    }
+
+    public boolean isCardFiltered(Card card) {
+        String selectedImportance = getSelectedImportance();
+        if (selectedImportance.isEmpty()) {
+            return true; // No filter applied, show all cards
+        } else {
+            return card.getImportance().equalsIgnoreCase(selectedImportance);
+        }
+    }
+
+    public String getSelectedImportance() {
+        if (importantBtn.isSelected()) {
+            return "Important";
+        } else if (maybeBtn.isSelected()) {
+            return "Maybe";
+        } else if (notImportantBtn.isSelected()) {
+            return "Not Important";
+        } else {
+            return "";
+        }
+    }
 }
